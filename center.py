65ba4be6138d81bfda81ee191f0e0dd8ea2ce959
paradauer <paradauer@tsn.at>
2021-05-02 14:15:40 +0200

    Serial mit Datenverarbeitung, aba behindert, müssn ma spata auf websockets umstelln

--- center.py
+++ center.py
@@ -1,25 +1,41 @@
 import serial
 import time
 
-ser = serial.Serial(
-    port='COM3',\
-    baudrate=9600,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-        timeout=0)
+class Sensor():
+    def __init__(self):
+        self.wertetabelle = []
+    
+    def appendwertetabelle(self, newTemp):
+        self.wertetabelle.append(newTemp)
 
+ser = serial.Serial(port='COM3', baudrate=9600, timeout=0)
 
 serCOM1 = serial.Serial(port='COM1', baudrate=9600, timeout=0)
 
-
 print("connected to: " + ser.portstr)
 
-while True:
-    line = ser.read(10)
 
-    print(line.decode("ascii"))
-    time.sleep(1)
+SensorCOM3 = Sensor()
+
+while True:
+    line = ser.readline()
+    line = line.decode("ascii")
+    a = line.split()
+
+    try: 
+        if a[0] == "SensorCOM3":
+            print(line)
+            line = line.split()
+            a = line[0] + ".appendwertetabelle(" + line[1] + ")"
+            eval(a)
+            print(SensorCOM3.wertetabelle)
+
+        elif line != " $":
+            print(line)
+
+    except:
+        continue
+    time.sleep(0.4)
     
 
 ser.close()
