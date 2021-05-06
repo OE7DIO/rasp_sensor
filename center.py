c4f40b94009639354ad4aaa221bd9d0bfe5ae7d9
paradauer <paradauer@tsn.at>
2021-05-06 15:44:24 +0200

    Subthreads werden jetzt auch gestoppt, wenn das Hauptprogramm gestoppt wird

--- center.py
+++ center.py
@@ -1,6 +1,11 @@
 import socket
 import msgpack
 import threading
+import time as t
+
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!D"
+statusRunning = True
 
 class Sensor():
     def __init__(self, name, ip, port):
@@ -22,23 +27,13 @@ class Sensor():
         self.__unpacker.feed(self.__buffer)
         for x in self.__unpacker:
             return x
-
     
     def addValue(self, newValue):
         self.__listOfValues.append(newValue)
 
     def printValues(self):
         print(self.__listOfValues)
-    
 
-
-PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!D"
-SERVER = "127.0.0.1"
-ADDR = (SERVER, PORT)
-
-SensorMils = Sensor("Mils", "127.0.0.1", 5050)
 
 def activate_sensor(Sensor, msg):
     Sensor.establishConnection()
@@ -46,21 +41,22 @@ def activate_sensor(Sensor, msg):
     thread.start()
 
 def collect_data(msg, Sensor):
-
-    while 1:
+    global statusRunning
+    while statusRunning:
         Sensor.send_msg(msg)
         x = Sensor.recv_msg()
         Sensor.addValue(x["value"])
         Sensor.printValues()
 
 
-try:
-    activate_sensor(SensorMils, "a Fleischinger Kaas")
-    #SensorMils.establishConnection()
-    while 1:
-        #collect_data("An schenen Messwert lieferns ma do!", SensorMils)
-        t.sleep(10)
-        pass
+if __name__ == "__main__":
+    SensorMils = Sensor("Mils", "127.0.0.1", 5050)
+    try:
+        activate_sensor(SensorMils, "a Fleischinger Kaas")
+        while 1:
+            t.sleep(10)
+            pass
 
-finally:
-    send(DISCONNECT_MESSAGE, SensorMils)
+    finally:
+        SensorMils.send_msg(DISCONNECT_MESSAGE)
+        statusRunning = False
